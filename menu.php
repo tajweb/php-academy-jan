cbf91ecc8967f0d62e504f64bccf5e789cb45588
Mohammad <tajweb.m@gmail.com>
2022-01-24 17:15:54 +0500

    Fixed register :)

--- menu.php
+++ menu.php
@@ -14,20 +14,20 @@ Cooking time: 25 minutes
 Servings: 8
 */
 function cookSquidMeatballs (
-    int $Tomato_juice, 
-    int $Vegetable_oil, 
+    int $tomato_juice, 
+    int $vegetable_oil, 
     int $egg, 
-    int $Flour, 
-    int $Rice, 
-    int $Onion, 
-    int $Squid)
+    int $flour, 
+    int $rice, 
+    int $onion, 
+    int $squid)
     {
         echo 
-        "1. $Squid grams of squid and $Onion onions need to be peeled and cut not very finely.
+        "1. $squid grams of squid and $onion onions need to be peeled and cut not very finely.
          2. And pass through a meat grinder.
-         3. Add $Rice grams of boiled rice, $egg egg, salt and pepper. Mix well.
-         4. Shape the minced meat into balls and bread them in $Flour tablespoons of flour. Fry in $Vegetable_oil tablespoons vegetable oil on all sides until golden brown.
-         5. Then pour $Tomato_juice glass of tomato juice and simmer for 10-15 minutes.
+         3. Add $rice grams of boiled rice, $egg egg, salt and pepper. Mix well.
+         4. Shape the minced meat into balls and bread them in $flour tablespoons of flour. Fry in $vegetable_oil tablespoons vegetable oil on all sides until golden brown.
+         5. Then pour $tomato_juice glass of tomato juice and simmer for 10-15 minutes.
          Garnish with mashed potatoes... and an unusual delicious dinner is ready! Bon Appetit!";
     }
 cookSquidMeatballs(1, 2, 1, 3, 150, 3, 600);
